125b6a492f004a7bec7f12cc9c175dc8e245c2ea
IndianShogun1 <shreyansh9383@gmail.com>
2018-03-21 12:57:00 +0530

    Update ListChildren.java

--- src/main/java/org/shreyansh/osgiDemo/htl/ListChildren.java
+++ src/main/java/org/shreyansh/osgiDemo/htl/ListChildren.java
@@ -14,10 +14,6 @@ import java.util.List;
 
 public class ListChildren  implements Use{
 
-//    @Reference
-//    SomeService someService;
-
-//    Iterator<Resource> children = null;
     List<Resource> children = null;
 
     String path;
@@ -26,32 +22,12 @@ public class ListChildren  implements Use{
     public void init(Bindings bindings) {
         SlingHttpServletRequest request = (SlingHttpServletRequest) bindings.get("request");
         path = request.getParameter("path");
-//        Resource resource = request.getResourceResolver().getResource(path);
-//        children = resource.listChildren();
         SlingScriptHelper slingScriptHelper = (SlingScriptHelper) bindings.get("sling");
         SomeService someService = slingScriptHelper.getService(SomeService.class);
         children = someService.listChildren(path);
-//        int count = 0;
-//        while(children.hasNext()) {
-//            count++;
-//            children.next();
-//        }
-//        System.out.println(count);
     }
 
     public List<Resource> getChildren() {
-//        int count = 0;
-//        while(children.hasNext()) {
-//            count++;
-//            children.next();
-//        }
-//        System.out.println(count);
-//        while (children.hasNext()) {
-//            Resource resource = children.next();
-//            ValueMap valueMap = resource.adaptTo(ValueMap.class);
-//            System.out.println(valueMap.get("title", "default"));
-//        }
-//        System.out.println(children);
         return children;
     }
 }
